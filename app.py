21ba370369855771a62ecb8acbba5ba4347b26d4
juliann2004 <juliannvegapsu@gmail.com>
2026-04-28 21:42:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     hostname = socket.gethostname()
-    # Version 1.0 (Blue Environment)
+    # Version 1.1 (Blue Environment)
     return {"version": "Blue (v1.1)", "pod": hostname}
